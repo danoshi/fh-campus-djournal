a84e935a985939fc9c7497c071fcb2c273cb87a1
Matthias Schmid-Kietreiber <matthias.schmid-kietreiber@protonmail.com>
2021-05-29 11:18:05 +0200

    set clicklistener for fab button in homefragment
    Took 5 minutes

--- app/src/main/java/fh/campus/djournal/fragments/HomeFragment.kt
+++ app/src/main/java/fh/campus/djournal/fragments/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import fh.campus.djournal.R
 import fh.campus.djournal.adapters.JournalListAdapter
@@ -27,6 +28,11 @@ class HomeFragment : Fragment() {
         binding.journalList.adapter = adapter // assign adapter to the recyclerView
 
         subscribeUI(adapter)
+
+        binding.addNewJournal.setOnClickListener {
+            Toast.makeText(requireContext(), "TODO: dialog to enter title and description for new Journal", Toast.LENGTH_SHORT).show()
+        }
+
         return binding.root
     }
 
