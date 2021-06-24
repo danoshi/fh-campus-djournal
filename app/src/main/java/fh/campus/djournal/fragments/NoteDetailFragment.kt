f96ff329d4f5509d5d4d41698b4a1575bac2eddc
Matthias Schmid-Kietreiber <matthias.schmid-kietreiber@protonmail.com>
2021-06-24 20:08:28 +0200

    implementation of some editor functions

--- app/src/main/java/fh/campus/djournal/fragments/NoteDetailFragment.kt
+++ app/src/main/java/fh/campus/djournal/fragments/NoteDetailFragment.kt
@@ -2,11 +2,13 @@ package fh.campus.djournal.fragments
 
 import android.os.Bundle
 import android.util.Log
+import android.util.Log.INFO
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.addCallback
+import androidx.core.text.toSpannable
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -18,9 +20,11 @@ import fh.campus.djournal.database.AppDatabase
 import fh.campus.djournal.databinding.FragmentNoteDetailBinding
 import fh.campus.djournal.models.Note
 import fh.campus.djournal.repositories.NoteRepository
+import fh.campus.djournal.utils.ToastMaker
 import fh.campus.djournal.viewmodels.NoteViewModel
 import fh.campus.djournal.viewmodels.NoteViewModelFactory
 import jp.wasabeef.richeditor.RichEditor
+import java.util.logging.Level.INFO
 
 class NoteDetailFragment : Fragment() {
     private lateinit var binding: FragmentNoteDetailBinding
@@ -40,14 +44,9 @@ class NoteDetailFragment : Fragment() {
         setHasOptionsMenu(true)
 
         mEditor = binding.editor
-        Log.i("backbutton", binding.editor.)
         mEditor.setEditorHeight(200);
         mEditor.setEditorFontSize(22);
         mEditor.setPadding(10, 10, 10, 10);
-        mEditor.setPlaceholder("Insert text here...");
-        var mPreview = binding.noteDetailText
-
-        mEditor.setOnTextChangeListener { text -> mPreview.setText(text) }
 
 
         val application = requireNotNull(this.activity).application
@@ -68,7 +67,8 @@ class NoteDetailFragment : Fragment() {
                 noteObj = note
                 binding.noteDetailName.setText(note.name)
                 binding.noteDetailDate.text = note.timestamp
-                binding.noteDetailText.setText(note.text)
+                mEditor.html = note.text
+//                binding.noteDetailText.setText(note.text)
                 journalId = note.journalIdOfNote
 
             }
@@ -79,15 +79,70 @@ class NoteDetailFragment : Fragment() {
             saveNoteDialog(
                 noteObj,
                 binding.noteDetailName.text.toString(),
-                binding.noteDetailText.text.toString(),
+                mEditor.html,
                 journalId,
             )
 
         }
 
+        val spannable = binding.noteDetailText
+        val a = spannable.selectionStart
+        val b = spannable.selectionEnd
+
+        binding.btn.setOnClickListener {
+            ToastMaker().toastMaker(requireContext(), spannable.length().toString())
+        }
+
+        binding.btn2.setOnClickListener {
+            ToastMaker().toastMaker(requireContext(), spannable.selectionStart.toString())
+        }
+
+        binding.btn3.setOnClickListener {
+            ToastMaker().toastMaker(requireContext(), spannable.selectionEnd.toString())
+        }
 
         binding.actionBold.setOnClickListener {
-            Log.i("backbutton", binding.editor.toString())
+            mEditor.setBold()
+        }
+
+        binding.actionItalic.setOnClickListener {
+            mEditor.setItalic()
+        }
+
+        binding.actionUnderline.setOnClickListener {
+            mEditor.setUnderline()
+        }
+
+        binding.actionAlignLeft.setOnClickListener {
+            mEditor.setAlignLeft()
+        }
+
+        binding.actionAlignRight.setOnClickListener {
+            mEditor.setAlignRight()
+        }
+
+        binding.actionAlignCenter.setOnClickListener {
+            mEditor.setAlignCenter()
+        }
+
+        binding.actionHeading1.setOnClickListener {
+            mEditor.setHeading(1)
+        }
+
+        binding.actionHeading2.setOnClickListener {
+            mEditor.setHeading(2)
+        }
+
+        binding.actionHeading3.setOnClickListener {
+            mEditor.setHeading(3)
+        }
+
+        binding.actionInsertBullets.setOnClickListener {
+            mEditor.setBullets()
+        }
+
+        binding.actionInsertNumbers.setOnClickListener {
+            mEditor.setNumbers()
         }
 
         return binding.root
@@ -97,19 +152,18 @@ class NoteDetailFragment : Fragment() {
         return when (item.itemId) {
             //TODO: normaly it should be R.id.***
             16908332 -> {
-//                saveNoteDialog(
-//                    noteObj,
-//                    binding.noteDetailName.text.toString(),
-//                    binding.noteDetailText.text.toString(),
-//                    journalId
-//                )
+                saveNoteDialog(
+                    noteObj,
+                    binding.noteDetailName.text.toString(),
+                    mEditor.html,
+                    journalId
+                )
                 true
             }
             else -> super.onOptionsItemSelected(item)
         }
 
     }
-
 
 
     private fun saveNoteDialog(
